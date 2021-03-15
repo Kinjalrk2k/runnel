acfc4dddf761c06e813fa08130f8e2ba102b0c5e
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2021-03-15 18:09:20 +0530

    added redux dev tools

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 
 import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import { CssBaseline } from "@material-ui/core";
@@ -10,7 +10,8 @@ import { deepPurple } from "@material-ui/core/colors";
 import App from "./components/App";
 import reducers from "./reducers";
 
-const store = createStore(reducers);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware()));
 
 const theme = createMuiTheme({
   palette: {
