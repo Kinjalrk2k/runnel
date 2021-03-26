38da55c0112f69183b319421b3898335ba7ed649
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2021-03-26 21:01:29 +0530

    form edit and init values done

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,18 +1,35 @@
+import _ from "lodash";
 import React from "react";
+import { Typography } from "@material-ui/core";
 import { connect } from "react-redux";
-import { fetchStream } from "../../actions";
+import { fetchStream, editStream } from "../../actions";
+import StreamForm from "./StreamForm";
 
 class StreamEdit extends React.Component {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
 
+  onSubmit = (formValues) => {
+    console.log(formValues);
+  };
+
   render() {
     if (!this.props.stream) {
       return <div>Loading...</div>;
     }
 
-    return <div>{this.props.stream.title}</div>;
+    return (
+      <>
+        <Typography variant="h3" component="h1" style={{ textAlign: "center" }}>
+          Edit your Stream
+        </Typography>
+        <StreamForm
+          onSubmit={this.onSubmit}
+          initialValues={_.pick(this.props.stream, "title", "description")}
+        />
+      </>
+    );
   }
 }
 
@@ -20,4 +37,6 @@ const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit);
+export default connect(mapStateToProps, { fetchStream, editStream })(
+  StreamEdit
+);
