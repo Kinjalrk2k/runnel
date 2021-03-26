a240f6d7e53e283c650a5e613fb3ea68ba64d426
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2021-03-26 20:49:18 +0530

    streamForm done and SreamCreate refractored

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,68 +1,24 @@
 import React from "react";
-import { Field, reduxForm } from "redux-form";
-import { Container, Button, Typography } from "@material-ui/core";
-import RenderInput from "./RenderInput";
+import { Typography } from "@material-ui/core";
 import { connect } from "react-redux";
 import { createStream } from "../../actions";
+import StreamForm from "./StreamForm";
 
 class StreamCreate extends React.Component {
   onSubmit = (formValues) => {
-    console.log(formValues);
     this.props.createStream(formValues);
   };
 
   render() {
     return (
-      <Container maxWidth="md">
+      <>
         <Typography variant="h3" component="h1" style={{ textAlign: "center" }}>
           Create a New Stream
         </Typography>
-        <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
-          <Field
-            component={RenderInput}
-            name="title"
-            label="Title"
-            fieldType="input"
-          />
-          <Field
-            component={RenderInput}
-            name="description"
-            label="Description"
-            fieldType="textarea"
-          />
-          <Button
-            variant="contained"
-            size="large"
-            color="primary"
-            type="submit"
-          >
-            Submit
-          </Button>
-        </form>
-      </Container>
+        <StreamForm onSubmit={this.onSubmit} />
+      </>
     );
   }
 }
 
-const validate = (formValues) => {
-  const errors = {};
-
-  if (!formValues.title) {
-    errors.title = "Your stream must have a title!";
-  }
-
-  if (!formValues.description) {
-    errors.description = "Your stream must have a brief description!";
-  }
-
-  return errors;
-};
-
-const formWrapped = reduxForm({
-  // config here!
-  form: "streamCreate", // name of the form
-  validate,
-})(StreamCreate);
-// reduxForm() is similar to connect()
-
-export default connect(null, { createStream })(formWrapped);
+export default connect(null, { createStream })(StreamCreate);
---
+++ src/components/streams/StreamForm.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { Field, reduxForm } from "redux-form";
+import { Container, Button, Typography } from "@material-ui/core";
+import RenderInput from "./RenderInput";
+
+class StreamForm extends React.Component {
+  onSubmit = (formValues) => {
+    this.props.onSubmit(formValues);
+  };
+
+  render() {
+    return (
+      <Container maxWidth="md">
+        <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
+          <Field
+            component={RenderInput}
+            name="title"
+            label="Title"
+            fieldType="input"
+          />
+          <Field
+            component={RenderInput}
+            name="description"
+            label="Description"
+            fieldType="textarea"
+          />
+          <Button
+            variant="contained"
+            size="large"
+            color="primary"
+            type="submit"
+          >
+            Submit
+          </Button>
+        </form>
+      </Container>
+    );
+  }
+}
+
+const validate = (formValues) => {
+  const errors = {};
+
+  if (!formValues.title) {
+    errors.title = "Your stream must have a title!";
+  }
+
+  if (!formValues.description) {
+    errors.description = "Your stream must have a brief description!";
+  }
+
+  return errors;
+};
+
+export default reduxForm({
+  // config here!
+  form: "streamForm", // name of the form
+  validate,
+})(StreamForm);
+// reduxForm() is similar to connect()
